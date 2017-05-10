de642bec7a50d3f1b6c22e9c3833c46adf8762f3
Maxim Mazurenko <maximmazurenko@rambler.ru>
2017-05-10 19:15:02 +0300

    Renamed ExceptionHandlingController

--- src/main/java/com/liberty/social/handler/ExceptionHandlingController.java
+++ src/main/java/com/liberty/social/handler/ExceptionHandlingController.java
@@ -1,17 +1,26 @@
 package com.liberty.social.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.social.RateLimitExceededException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
-public class RatelimitExceptionHandlingController {
+public class ExceptionHandlingController {
 
 	@ExceptionHandler(RateLimitExceededException.class)
 	public String rateLimitExceeded(RateLimitExceededException e, Model model) {
 		model.addAttribute("providerId", e.getProviderId());
 		return "error/ratelimit";
 	}
-	
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String pageNotFound(NoHandlerFoundException ex, Model model) {
+		model.addAttribute("url", ex.getRequestURL());
+		return "error/pagenotfound";
+	}
 }
